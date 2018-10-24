5c6f75ed82e90f57c724ee0f9ec0ff9e2c7e09bb
aubristahl <aubristahl@gmail.com>
2018-10-24 18:14:07 -0500

    Created contact form. Added listview to skills pg. Still need Icon on contact form.

--- AubriBioApp/AubriBioApp/ContactPage.xaml.cs
+++ AubriBioApp/AubriBioApp/ContactPage.xaml.cs
@@ -7,6 +7,33 @@ namespace AubriBioApp
 {
     public partial class ContactPage : ContentPage
     {
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+
+            if (string.IsNullOrEmpty(fullNameEntry.Text))
+            {
+                DisplayAlert("Error!", "Enter name", "Ok!");
+                return;
+            }
+
+
+            if (string.IsNullOrEmpty(emailEntry.Text))
+            {
+                DisplayAlert("Error!", "Enter email", "Ok!");
+                return;
+            }
+
+
+            if (string.IsNullOrEmpty(phoneEntry.Text))
+            {
+                DisplayAlert("Error!", "Enter phone", "Ok!");
+                return;
+            }
+
+            DisplayAlert("Hello!", "Your message has been sent.","Ok!");
+           
+        }
+
         public ContactPage()
         {
             InitializeComponent();
--- AubriBioApp/AubriBioApp/skillsPage.xaml.cs
+++ AubriBioApp/AubriBioApp/skillsPage.xaml.cs
@@ -7,6 +7,11 @@ namespace AubriBioApp
 {
     public partial class skillsPage : ContentPage
     {
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
         public skillsPage()
         {
             InitializeComponent();
