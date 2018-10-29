a594f7dd48c2dba300449c1ca9f292911e13fd40
aubristahl <aubristahl@gmail.com>
2018-10-29 18:23:02 -0500

    Created skills page headers in ai Started skills page

--- AubriBioApp/AubriBioApp/skillsPage.xaml.cs
+++ AubriBioApp/AubriBioApp/skillsPage.xaml.cs
@@ -1,20 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Collections.ObjectModel;
 using Xamarin.Forms;
 
 namespace AubriBioApp
 {
     public partial class skillsPage : ContentPage
     {
+        public ObservableCollection<Skill> Skills { get; set; }
         void Handle_Clicked(object sender, System.EventArgs e)
-        {
-            throw new NotImplementedException();
+        {    
         }
 
         public skillsPage()
         {
             InitializeComponent();
+            this.Skills = new ObservableCollection<Skill>();
+            this.Skills.Add(new Skill {Icon = "BackendButton.png"});
+            this.Skills.Add(new Skill {Description = "skill1", Name = "- C++" });
+            this.Skills.Add(new Skill {Description = "skill2", Name = "- C#" });
+
+            skills.ItemsSource = this.Skills;
         }
+    }
+
+    public class Skill
+    {
+        public string Icon { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
     }
 }
